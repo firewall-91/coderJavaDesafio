358d731f7c61c55cff3d7c157890175993021d0d
Kevin Dalies <kevdalies@gmail.com>
2022-08-31 23:59:08 -0300

    commit desafio fix

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const NOMBRE = prompt ("Ingresa tu nombre")
-const EDAD = prompt ("Ingresa tu edad")
-
-if(EDAD >= 18)  {
-    alert(NOMBRE + " " + "estas habilitado a votar")
+if(NOMBRE != "") {
+    const NUMERO_CALIFICACIONES = prompt ("Ingresa la cantidad de calificaciones")
+    let sumatoria = 0
+    let promedio = 0
+    for(let index = 1; index <= NUMERO_CALIFICACIONES; index++) {
+        const CALIFICACION = prompt ("Ingrese la calificación" + " " + index)
+        sumatoria = sumatoria + parseFloat(CALIFICACION)
+    }
+    
+    promedio = sumatoria / NUMERO_CALIFICACIONES
+    
+    if(promedio >= 6)  {
+        alert(NOMBRE + " " + "¡Felicitaciones aprobaste!" + " " + "Tu nota es: " + " " + promedio.toFixed(0))
+    }
+    else {
+        alert(NOMBRE + " " + "No aprobaste." + " " + "Tu nota es: " + " " + promedio.toFixed(0))
+    }
 }
-else {
-    alert(NOMBRE + " " + "no estas habilitado a votar")
+
+let valor = prompt("ESC para salir")
+
+while(valor != "ESC") {
+    alert ("El usuario ingresó: " + valor)
+    valor = prompt("ESC para salir")
 }
