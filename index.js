d4d249bc9af3ee54f1f16fa8d21967a2b5ac655d
Kevin Dalies <kevdalies@gmail.com>
2022-09-05 20:25:37 -0300

    Desafio Fix

--- index.js
+++ index.js
@@ -1,49 +1,51 @@
+/* La constante es usada para personalizar la función con el nombre de la persona. */
 const NOMBRE = prompt ("Ingresa tu nombre")
 
+/* Función promedio*/ 
 function promedio(numeroUno, numeroDos) {
     let resultado = (numeroUno + numeroDos) / 2;
     return resultado;
-  }
-  
-  function mostrarResultado(resultado) {
+}
+
+function mostrarResultado(resultado) {
     alert(NOMBRE + "." + " " + "El resultado del promedio es: " + resultado);
-  }
-  
-  function mostrarMenu() {
+}
+
+function mostrarMenu() {
     let opcion = prompt(
         "Bienvenido" + " " + NOMBRE, " " + "selecciona una opción o S para salir\n1. Promedio"
     );
     return opcion;
-  }
-  
-  function calculadora() {
+}
+
+function calculadora() {
     let opcionSeleccionada = mostrarMenu();
     while (opcionSeleccionada !== "S") {
-      if (opcionSeleccionada !== "") {
+    if (opcionSeleccionada !== "") {
         opcionSeleccionada = parseInt(opcionSeleccionada);
-  
+
         //isNaN nos sirve para saber si el contenido de una variable es NaN
         if (!isNaN(opcionSeleccionada)) {
-          let numeroUno = parseFloat(prompt("Ingrese el primer numero"));
-          let numeroDos = parseFloat(prompt("Ingrese el segundo numero"));
-          switch (opcionSeleccionada) {
+            let numeroUno = parseFloat(prompt("Ingrese el primer numero"));
+            let numeroDos = parseFloat(prompt("Ingrese el segundo numero"));
+            switch (opcionSeleccionada) {
             case 1:
-              let resultadoPromedio = promedio(numeroUno, numeroDos);
-              mostrarResultado(resultadoPromedio)
-              break;  
+                let resultadoPromedio = promedio(numeroUno, numeroDos);
+                mostrarResultado(resultadoPromedio)
+                break;  
             
-              default:
-              alert("Opcion Incorrecta");
-              break;
-          }
+                default:
+                alert("Opcion Incorrecta");
+                break;
+            }
         } else {
-          alert("Ingresó una letra");
+            alert("Ingresó una letra");
         }
-      } else {
+    }   else {
         alert("Seleccione la opción");
-      }
-      opcionSeleccionada = mostrarMenu();
+        }
+        opcionSeleccionada = mostrarMenu();
     }
-  }
-  
-  calculadora();
+}
+
+calculadora();
