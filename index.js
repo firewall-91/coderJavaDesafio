957e16a18c8084e869c3f837929ba0c91c061782
Kevin Dalies <kevdalies@gmail.com>
2022-09-05 16:20:12 -0300

    Desafio

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
 const NOMBRE = prompt ("Ingresa tu nombre")
-if(NOMBRE != "") {
-    const NUMERO_CALIFICACIONES = prompt ("Ingresa la cantidad de calificaciones")
-    let sumatoria = 0
-    let promedio = 0
-    for(let index = 1; index <= NUMERO_CALIFICACIONES; index++) {
-        const CALIFICACION = prompt ("Ingrese la calificación" + " " + index)
-        sumatoria = sumatoria + parseFloat(CALIFICACION)
-    }
-    
-    promedio = sumatoria / NUMERO_CALIFICACIONES
-    
-    if(promedio >= 6)  {
-        alert(NOMBRE + " " + "¡Felicitaciones aprobaste!" + " " + "Tu nota es: " + " " + promedio.toFixed(0))
-    }
-    else {
-        alert(NOMBRE + " " + "No aprobaste." + " " + "Tu nota es: " + " " + promedio.toFixed(1))
-    }
-}
 
-//Si NOMBRE vacio entonces entra al while
-
-let valor = prompt("ESC para salir")
-
-while(valor != "ESC") {
-    alert ("Comando incorrecto")
-    valor = prompt("Escribe ESC para salir")
-}
+function promedio(numeroUno, numeroDos) {
+    let resultado = (numeroUno + numeroDos) / 2;
+    return resultado;
+  }
+  
+  function mostrarResultado(resultado) {
+    alert(NOMBRE + "." + " " + "El resultado del promedio es: " + resultado);
+  }
+  
+  function mostrarMenu() {
+    let opcion = prompt(
+        "Bienvenido" + " " + NOMBRE, " " + "selecciona una opción o S para salir\n1. Promedio"
+    );
+    return opcion;
+  }
+  
+  function calculadora() {
+    let opcionSeleccionada = mostrarMenu();
+    while (opcionSeleccionada !== "S") {
+      if (opcionSeleccionada !== "") {
+        opcionSeleccionada = parseInt(opcionSeleccionada);
+  
+        //isNaN nos sirve para saber si el contenido de una variable es NaN
+        if (!isNaN(opcionSeleccionada)) {
+          let numeroUno = parseFloat(prompt("Ingrese el primer numero"));
+          let numeroDos = parseFloat(prompt("Ingrese el segundo numero"));
+          switch (opcionSeleccionada) {
+            case 1:
+              let resultadoPromedio = promedio(numeroUno, numeroDos);
+              mostrarResultado(resultadoPromedio)
+              break;  
+            
+              default:
+              alert("Opcion Incorrecta");
+              break;
+          }
+        } else {
+          alert("Ingresó una letra");
+        }
+      } else {
+        alert("Seleccione la opción");
+      }
+      opcionSeleccionada = mostrarMenu();
+    }
+  }
+  
+  calculadora();
