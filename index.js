da52874f75901d73676681bb2f23ed08e44942ca
Kevin Dalies <kevdalies@gmail.com>
2022-09-14 12:49:55 -0300

    Desafio primera entrega

--- index.js
+++ index.js
@@ -1,60 +1,165 @@
-/* La constante es usada para personalizar la función con el nombre de la persona. */
+/* Nota mental: dividir el problema en partes pequeñas */
 
-const NOMB = prompt ("Ingresa tu nombre")
-const APE = prompt ("Ingresa tu apellido")
-/* Función promedio*/ 
-function cuenta(numeroUno, numeroDos) {
-    let resultado = (numeroUno / numeroDos) * 0.12 + (numeroUno / numeroDos);
-    return resultado;
-}
-
-/* En consola muestre con Arrays los prestamos realizados */
-function mostrarResultado(resultado) {
-    alert(NOMB + "." + " " + "El monto por cuota es: " + " " + "$" + resultado);
-    const arrayNombresPrestamo = [NOMB, APE, resultado];
-    console.log(arrayNombresPrestamo)
-    const obj = Object.assign({}, arrayNombresPrestamo);
-    console.log(obj)
-}
-
-/* Base de datos manual con objeto*/
-
-function mostrarMenu() {
-    let opcion = prompt(
-        "Selecciona una opción o S para salir\n1 Prestamo"
-    );
-    return opcion;
-}
-
-function calculadora() {
-    let opcionSeleccionada = mostrarMenu();
-    while (opcionSeleccionada !== "S") {
-    if (opcionSeleccionada !== "") {
-        opcionSeleccionada = parseInt(opcionSeleccionada);
-
-        //isNaN nos sirve para saber si el contenido de una variable es NaN
-        if (!isNaN(opcionSeleccionada)) {
-            let numeroUno = parseFloat(prompt("Ingrese monto en pesos a retirar"));
-            let numeroDos = parseFloat(prompt("Ingrese el numero de cuotas"));
-            switch (opcionSeleccionada) {
-            case 1:
-                let resultadoCuenta = cuenta(numeroUno, numeroDos);
-                mostrarResultado(resultadoCuenta)
-                break;  
-            
-                default:
-                alert("Opcion Incorrecta");
-                break;
-
-            }
-        } else {
-            alert("Ingresó una letra");
-        }
-    }   else {
-        alert("Seleccione la opción");
-        }
-        opcionSeleccionada = mostrarMenu();
+// Variables
+const baseDeDatos = [
+    {
+        id: 1,
+        nombre: 'Ralph Lauren Polo Blue',
+        precio: 6050,
+        imagen: './media/poloBlue.jpg'
+    },
+    {
+        id: 2,
+        nombre: 'Carolina Herrera 212 Vip Men',
+        precio: 5649,
+        imagen: './media/cH212VipMen.jpg'
+    },
+    {
+        id: 3,
+        nombre: 'Carolina Herrera Good Girl',
+        precio: 6990,
+        imagen: './media/cHGoodGirl.jpg'
+    },
+    {
+        id: 4,
+        nombre: 'Lancome La Vie Est Belle',
+        precio: 7890,
+        imagen: './media/laVieEstBelle.jpg'
     }
-}
+];
 
-calculadora();
+/* Podria agregar mas elementos pero de momento voy a usar estos para Coderhouse */
+
+let carrito = [];
+const divisa = '$';
+const DOMitems = document.querySelector('#items');
+const DOMcarrito = document.querySelector('#carrito');
+const DOMtotal = document.querySelector('#total');
+const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+// Funciones
+/**
+ * Sketch de todos los productos a partir de la base de datos. No confundir con el carrito
+ */
+function renderizarProductos() {
+    baseDeDatos.forEach((info) => {
+        // Estructura
+        const miNodo = document.createElement('div');
+        miNodo.classList.add('card', 'col-sm-4');
+        // Body
+        const miNodoCardBody = document.createElement('div');
+        miNodoCardBody.classList.add('card-body');
+        // Titulo
+        const miNodoTitle = document.createElement('h5');
+        miNodoTitle.classList.add('card-title');
+        miNodoTitle.textContent = info.nombre;
+        // Imagen
+        const miNodoImagen = document.createElement('img');
+        miNodoImagen.classList.add('img-fluid');
+        miNodoImagen.setAttribute('src', info.imagen);
+        // Precio
+        const miNodoPrecio = document.createElement('p');
+        miNodoPrecio.classList.add('card-text');
+        miNodoPrecio.textContent = `${divisa}${info.precio}`;
+        // Boton 
+        const miNodoBoton = document.createElement('button');
+        miNodoBoton.classList.add('btn', 'btn-primary');
+        miNodoBoton.textContent = '+';
+        miNodoBoton.setAttribute('marcador', info.id);
+        miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+        // Insertamos
+        miNodoCardBody.appendChild(miNodoImagen);
+        miNodoCardBody.appendChild(miNodoTitle);
+        miNodoCardBody.appendChild(miNodoPrecio);
+        miNodoCardBody.appendChild(miNodoBoton);
+        miNodo.appendChild(miNodoCardBody);
+        DOMitems.appendChild(miNodo);
+    });
+}
+/**
+ * Evento para añadir un producto al carrito de la compra
+ */
+function anyadirProductoAlCarrito(evento) {
+    // Añadimos el nodo a nuestro carrito
+    carrito.push(evento.target.getAttribute('marcador'))
+    // Actualizamos el carrito 
+    renderizarCarrito();
+}
+/**
+ * Sketch de todos los productos guardados en el carrito
+ */
+function renderizarCarrito() {
+    // Vaciamos todo el html
+    DOMcarrito.textContent = '';
+    // Quitamos los duplicados
+    const carritoSinDuplicados = [...new Set(carrito)];
+    // Generamos los Nodos a partir de carrito
+    carritoSinDuplicados.forEach((item) => {
+        // Obtenemos el item que necesitamos de la variable base de datos
+        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+            // ¿Coincide las id? Solo puede existir un caso
+            return itemBaseDatos.id === parseInt(item);
+        });
+        // Cuenta el número de veces que se repite el producto(aca me tranque)
+        const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+            // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
+            return itemId === item ? total += 1 : total;
+        }, 0);
+        // Creamos el nodo del item del carrito
+        const miNodo = document.createElement('li');
+        miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+        miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${divisa}${miItem[0].precio}`;
+        // Boton de borrar
+        const miBoton = document.createElement('button');
+        miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+        miBoton.textContent = 'X';
+        miBoton.style.marginLeft = '1rem';
+        miBoton.dataset.item = item;
+        miBoton.addEventListener('click', borrarItemCarrito);
+        // Mezclamos nodos
+        miNodo.appendChild(miBoton);
+        DOMcarrito.appendChild(miNodo);
+    });
+    // Renderizamos el precio total en el HTML
+    DOMtotal.textContent = calcularTotal();
+}
+/**
+ * Evento para borrar un elemento del carrito
+ */
+function borrarItemCarrito(evento) {
+    // Obtenemos el producto ID que hay en el boton pulsado
+    const id = evento.target.dataset.item;
+    // Borramos todos los productos
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== id;
+    });
+    // volvemos a renderizar
+    renderizarCarrito();
+}
+/**
+ * Calcula el precio total teniendo en cuenta los productos repetidos
+ */
+function calcularTotal() {
+    // Recorremos el array del carrito 
+    return carrito.reduce((total, item) => {
+        // De cada elemento obtenemos el precio
+        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+            return itemBaseDatos.id === parseInt(item);
+        });
+        // Los sumamos al total
+        return total + miItem[0].precio;
+    }, 0).toFixed(0);
+}
+/**
+ * Varia el carrito y vuelve a dibujarlo
+ */
+function vaciarCarrito() {
+    // Limpiamos los productos guardados
+    carrito = [];
+    // Renderizamos los cambios
+    renderizarCarrito();
+}
+// Eventos
+DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+// Inicio
+renderizarProductos();
+renderizarCarrito();
